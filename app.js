2b9c7c50b99d296d9666a03b2586799fbd850cff
Thiago Dias <nullck@gmail.com>
2017-04-01 12:27:13 -0300

    test

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ var jsonfile = require('jsonfile')
 
 var file = '/var/log/app.log'
 var date = new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '')
-var obj = "{'@message': 'I am Groot and you is?'}"
+var obj = "{'@message': 'I am Groot and you is? ok great!!'}"
 
 jsonfile.writeFileSync(file, obj, {flag: 'a'} )
